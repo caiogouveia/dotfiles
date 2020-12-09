e1876c0f91b4ec544ef96cad4344d08b13b42df6
Caio Gouveia <caiomdm@gmail.com>
2020-12-08 22:02:40 -0300

    lots of modifications to vimrc to use nvim on windows wsl, tmux modifications for new version

--- alias.sh
+++ alias.sh
@@ -1,18 +1,24 @@
 # Alias
 # ============================================================================>
 alias c="clear"
+alias cls="clear"
+alias vim="nvim"
 alias subl='open -a "/Applications/Sublime Text.app"'
 alias atom='open -a "/Applications/Atom.app"'
 alias macvim='open -a "/Applications/Macvim.app"'
-alias ls='ls -G'
-alias ll="ls -ls"
-alias llm="ls -lsh"
-alias la="ls -la"
-alias lla="ls -la"
+alias ls='ls -G --color=auto'
+alias ll="ls -ls --color=auto"
+alias llm="ls -lsh --color=auto"
+alias la="ls -la --color=auto"
+alias lla="ls -la --color=auto"
+# mac mysql
 alias mysql="sudo /usr/local/mysql/support-files/mysql.server"
+# rails/ruby
 alias rs="rails s"
 alias rc="rails console"
 alias migrate="rake db:drop && rake db:create && rake db:migrate && rake db:seed"
 alias killruby='killall -9 ruby'
 alias killrudy='killruby'
 alias kr="killruby"
+# windows WSL
+alias open='powershell.exe /c start'
