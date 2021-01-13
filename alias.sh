79248f881bca513b430d963bf05b1d5fa59b13a9
Caio Gouveia <caiomdm@gmail.com>
2021-01-12 21:22:16 -0300

    removed mac alias

--- alias.sh
+++ alias.sh
@@ -4,17 +4,12 @@ alias c="clear"
 alias cls="clear"
 alias vim="nvim"
 alias ovim="vim"
-alias subl='open -a "/Applications/Sublime Text.app"'
-alias atom='open -a "/Applications/Atom.app"'
-alias macvim='open -a "/Applications/Macvim.app"'
 alias ls='ls -G --color=auto'
 alias ll="ls -ls --color=auto"
 alias llm="ls -lsh --color=auto"
 alias la="ls -la --color=auto"
 alias lla="ls -la --color=auto"
 alias more="less"
-# mac mysql
-alias mysql="sudo /usr/local/mysql/support-files/mysql.server"
 # rails/ruby
 alias rs="rails s"
 alias rc="rails console"
