734a8b1fb3922d7384cddd47117f725d97b443c1
Caio Gouveia <caiomdm@gmail.com>
2021-09-19 11:54:58 -0300

    modified alias.sh on laravel alias

--- alias.sh
+++ alias.sh
@@ -10,19 +10,27 @@ alias llm="ls -lsh --color=auto"
 alias la="ls -la --color=auto"
 alias lla="ls -la --color=auto"
 alias more="less"
+
 # rails/ruby
 alias rs="rails s"
 alias rc="rails console"
-alias migrate="rake db:drop && rake db:create && rake db:migrate && rake db:seed"
+alias rmigrate="rake db:drop && rake db:create && rake db:migrate && rake db:seed"
 alias killruby='killall -9 ruby'
 alias killrudy='killruby'
 alias kr="killruby"
+
 # windows WSL
 alias open='powershell.exe /c start'
 alias subl='"/mnt/c/Program Files/Sublime Text/subl.exe"'
+
 # Laravel
-alias art="php artisan"
+alias artisan="php artisan"
+alias art="artisan"
 alias dbreset="php artisan migrate:fresh --seed"
+alias dbseed="dbreset"
+alias lseed="dbreset"
 alias tinker="php artisan tinker"
-alias serve="php artisan serve"
-alias migrate="php artisan migrate"
+alias laraserve="php artisan serve"
+alias lserve="laraserve"
+alias lmigrate="php artisan migrate"
+alias laramigrate="lmigrate"
