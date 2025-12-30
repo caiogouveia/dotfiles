2929c11fd162979eb64b78310d18869f345b6c77
Caio Gouveia <caiomdm@gmail.com>
2025-12-30 10:57:38 -0300

    adiciona keymaps ao lsp

--- nvim-prime/lua/theprimeagen/lazy/lsp.lua
+++ nvim-prime/lua/theprimeagen/lazy/lsp.lua
@@ -29,6 +29,25 @@ return {
 
         require("fidget").setup({})
         require("mason").setup()
+
+        -- Keymaps do LSP
+        vim.api.nvim_create_autocmd('LspAttach', {
+            group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+            callback = function(ev)
+                local opts = { buffer = ev.buf }
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                vim.keymap.set('n', '<leader>vws', vim.lsp.buf.workspace_symbol, opts)
+                vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float, opts)
+                vim.keymap.set('n', '[d', vim.diagnostic.goto_next, opts)
+                vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, opts)
+                vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action, opts)
+                vim.keymap.set('n', '<leader>vrr', vim.lsp.buf.references, opts)
+                vim.keymap.set('n', '<leader>vrn', vim.lsp.buf.rename, opts)
+                vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
+            end,
+        })
+
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
@@ -138,5 +157,14 @@ return {
                 prefix = "",
             },
         })
+
+        -- Bordas para hover e signature help
+        local handlers = {
+            ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
+            ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
+        }
+
+        vim.lsp.handlers["textDocument/hover"] = handlers["textDocument/hover"]
+        vim.lsp.handlers["textDocument/signatureHelp"] = handlers["textDocument/signatureHelp"]
     end
 }
