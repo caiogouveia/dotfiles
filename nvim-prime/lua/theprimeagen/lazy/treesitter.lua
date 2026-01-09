f7b09d0bfe09de3245636f24ab3ece9996de5db0
Caio Gouveia <caiomdm@gmail.com>
2026-01-09 14:28:41 -0300

    remove go

--- nvim-prime/lua/theprimeagen/lazy/treesitter.lua
+++ nvim-prime/lua/theprimeagen/lazy/treesitter.lua
@@ -1,27 +1,26 @@
-return {}
--- return {
---     "nvim-treesitter/nvim-treesitter",
---     build = ":TSUpdate",
---     event = { "BufReadPost", "BufNewFile" },
---     config = function()
---         local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
---         if not status_ok then
---             return
---         end
---         treesitter.setup({
---             ensure_installed = {
---                 "vimdoc", "javascript", "typescript", "tsx", "c", "lua", "rust",
---                 "jsdoc", "bash", "markdown", "markdown_inline"
---             },
---             sync_install = false,
---             auto_install = true,
---             indent = {
---                 enable = true
---             },
---             highlight = {
---                 enable = true,
---                 additional_vim_regex_highlighting = { "markdown" },
---             },
---         })
---     end
--- }
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+        local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
+        if not status_ok then
+            return
+        end
+        treesitter.setup({
+            ensure_installed = {
+                "vimdoc", "javascript", "typescript", "tsx", "c", "lua", "rust",
+                "jsdoc", "bash", "markdown", "markdown_inline"
+            },
+            sync_install = false,
+            auto_install = true,
+            indent = {
+                enable = true
+            },
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = { "markdown" },
+            },
+        })
+    end
+}
