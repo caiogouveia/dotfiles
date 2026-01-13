507226e960c310d3e71f3061773ca3bc87ab8c02
Caio Gouveia <caiomdm@gmail.com>
2026-01-13 14:34:35 -0300

    adiciona config do colorizer

--- nvim-prime/lua/theprimeagen/lazy/colorizer.lua
+++ nvim-prime/lua/theprimeagen/lazy/colorizer.lua
@@ -1,7 +1,20 @@
 return {
     'norcalli/nvim-colorizer.lua',
     config = function()
-        require('colorizer').setup()
+        require('colorizer').setup({
+            '*',
+        }, {
+            RGB = true,
+            RRGGBB = true,
+            names = true,
+            RRGGBBAA = true,
+            AARRGGBB = true,
+            rgb_fn = true,
+            hsl_fn = true,
+            css = true,
+            css_fn = true,
+            mode = 'background',
+        })
     end
 }
 
