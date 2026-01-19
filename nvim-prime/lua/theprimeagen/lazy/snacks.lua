a546624beffe7ecd79b3e3b1b71e28d1028a9ef2
Caio Gouveia <caiomdm@gmail.com>
2026-01-19 14:45:01 -0300

    fix init error

--- nvim-prime/lua/theprimeagen/lazy/snacks.lua
+++ nvim-prime/lua/theprimeagen/lazy/snacks.lua
@@ -1,19 +1,14 @@
 return {
-    {
-        "folke/snacks.nvim",
-        priority = 1000,
-        lazy = false,
-        ---@type snacks.config
-        opts = {
-            dashboard = {
-                enabled = true,
-                sections = {
-                    { section = "header" },
-                    { section = "keys", gap = 1, padding = 1 },
-                    { section = "startup" },
-                },
-                formats = {
-                    header = [[
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		---@type snacks.config
+		opts = {
+			dashboard = {
+				enabled = true,
+				preset = {
+					header = [[
 ████████╗██████╗  █████╗ ███████╗███████╗███████╗
 ╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
    ██║   ██████╔╝███████║███████╗███████╗█████╗
@@ -21,18 +16,64 @@ return {
    ██║   ██║  ██║██║  ██║███████║███████║███████╗
    ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝
 ]],
-                },
-            },
-        },
-        keys = {
-            { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
-            { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
-            { "<leader>gB", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
-            { "<leader>gb", function() Snacks.gitbrowse() end, desc = "Git Browse" },
-            { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
-            { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
-            { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
-        },
-    }
+				},
+				sections = {
+					{ section = "header" },
+					{ section = "keys", gap = 1, padding = 1 },
+					{ section = "startup" },
+				},
+			},
+		},
+		keys = {
+			{
+				"<leader>bd",
+				function()
+					Snacks.bufdelete()
+				end,
+				desc = "Delete Buffer",
+			},
+			{
+				"<leader>gg",
+				function()
+					Snacks.lazygit()
+				end,
+				desc = "Lazygit",
+			},
+			{
+				"<leader>gB",
+				function()
+					Snacks.git.blame_line()
+				end,
+				desc = "Git Blame Line",
+			},
+			{
+				"<leader>gb",
+				function()
+					Snacks.gitbrowse()
+				end,
+				desc = "Git Browse",
+			},
+			{
+				"<leader>gf",
+				function()
+					Snacks.lazygit.log_file()
+				end,
+				desc = "Lazygit Current File History",
+			},
+			{
+				"<leader>gl",
+				function()
+					Snacks.lazygit.log()
+				end,
+				desc = "Lazygit Log (cwd)",
+			},
+			{
+				"<leader>un",
+				function()
+					Snacks.notifier.hide()
+				end,
+				desc = "Dismiss All Notifications",
+			},
+		},
+	},
 }
-
