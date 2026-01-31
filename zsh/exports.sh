55b4e7d7ec9749ab4a38a1e839adca3c541eca5a
Caio Gouveia <caiomdm@gmail.com>
2026-01-31 16:27:37 -0300

    add utf8 to exports

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -1,8 +1,8 @@
 # UTF-8 Encoding
-# export LC_ALL=en_US.UTF-8
-# export LANG=en_US.UTF-8
-# export LC_CTYPE=en_US.UTF-8
-# export LANGUAGE="en_US.UTF-8"
+export LC_ALL=pt_BR.UTF-8
+export LANG=pt_BR.UTF-8
+export LC_CTYPE=pt_BR.UTF-8
+export LANGUAGE="pt_BR.UTF-8"
 
 export EDITOR='nvim'
 
